057aa86a406a9ef73d9df6cf3dfa7bb2454d36ef
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-02-12 20:51:29 -0300

    Add BanlistTCGUSWorker to the import list

--- workers/__init__.py
+++ workers/__init__.py
@@ -1,6 +1,8 @@
+from workers.banlist_tcg_us_worker import BanlistTCGUSWorker
 from workers.worker_register import worker_register
 
 
 __all__ = [
+    'BanlistTCGUSWorker',
     'worker_register',
 ]
