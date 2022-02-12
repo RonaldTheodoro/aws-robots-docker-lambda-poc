331488ace6c8729ee6475d9ab18903c869c8d72d
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-02-12 13:59:11 -0300

    Remove watchtower

--- settings/logger.py
+++ settings/logger.py
@@ -1,15 +1,8 @@
 import logging
 
-import watchtower
-
-from settings.settings import settings
-
 
 logging.basicConfig(
     format='%(asctime)s [%(process)d] %(levelname)s %(name)s: %(message)s',
     level=logging.DEBUG
 )
 logger = logging.getLogger('robots poc')
-
-if settings.is_aws:
-    logger.addHandler(watchtower.CloudWatchLogHandler())
