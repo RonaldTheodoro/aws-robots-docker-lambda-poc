965d3ea71b27c06fae306e365a78946ee190d30d
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-02-12 13:01:00 -0300

    Set logger to debug level

--- settings/logger.py
+++ settings/logger.py
@@ -1,4 +1,7 @@
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s [%(process)d] %(levelname)s %(name)s: %(message)s',
+    level=logging.DEBUG
+)
 logger = logging.getLogger('robots poc')
