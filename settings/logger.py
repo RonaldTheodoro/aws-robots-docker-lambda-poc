c8dda3dc5741168dc3dcf47680d97860f77d553a
Ronald Theodoro <ronald@keycash.io>
2022-07-07 17:38:45 -0300

    Adiciona a flag force no logger

--- settings/logger.py
+++ settings/logger.py
@@ -3,6 +3,7 @@ import logging
 
 logging.basicConfig(
     format='%(asctime)s [%(process)d] %(levelname)s %(name)s: %(message)s',
-    level=logging.DEBUG
+    level=logging.DEBUG,
+    force=True
 )
 logger = logging.getLogger('workers poc')
