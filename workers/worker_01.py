c945f761299ae7c2349131375549d17b474455ea
Ronald Theodoro <ronald@keycash.io>
2022-07-26 13:53:49 -0300

    Envia mensagem para a fila

--- workers/worker_01.py
+++ workers/worker_01.py
@@ -1,8 +1,13 @@
-from settings import logger
+import json
 
+import boto3
+
+from settings import logger
 from workers.base_worker import BaseWorker
 from workers.worker_register import worker_register
 
+
+sqs_client = boto3.client('sqs')
 
 @worker_register
 class Worker01(BaseWorker):
@@ -11,3 +16,12 @@ class Worker01(BaseWorker):
     def run(self, event, context):
         logger.info('Worker 01')
         logger.info('Event: %s', event)
+
+        msg = json.loads(event['body'])
+
+        response = sqs_client.send_message(
+            QueueUrl='https://sqs.us-east-1.amazonaws.com/986806511625/worker_cron',
+            MessageBody=json.dumps(msg),
+        )
+
+        logger.info('SQS Response: %s', response)
