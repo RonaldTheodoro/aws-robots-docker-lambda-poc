b6129821cd59d941c67934d337633d61fbc5f2af
Ronald Theodoro <ronald@keycash.io>
2022-07-26 15:39:03 -0300

    Define o nivel dos loggers de terceiros

--- settings/logger.py
+++ settings/logger.py
@@ -7,3 +7,21 @@ logging.basicConfig(
     force=True
 )
 logger = logging.getLogger('workers poc')
+
+
+loggers_name = [
+    'boto3',
+    'botocore',
+    'botocore.hooks',
+    'botocore.utils',
+    'botocore.credentials',
+    'botocore.loaders',
+    'botocore.endpoint',
+    'botocore.client',
+    'nose',
+    'urllib3.connectionpool',
+]
+
+for logger_name in loggers_name:
+    logging.getLogger(logger_name).setLevel(logging.INFO)
+
